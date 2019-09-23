029edfe3b81f7844bcd15e854fa4353ad1116c1c
ramon0306 <50596238+ramon0306@users.noreply.github.com>
2019-09-23 13:25:19 -0300

    Add files via upload

--- AjaxSync.js
+++ AjaxSync.js
@@ -1,11 +1,19 @@
+
 const getFotoGithub = function (username) {
+    var username = document.getElementById("username").value;
     var url = 'https://api.github.com/users/' + username;
 
     var ajax = new XMLHttpRequest();
-    ajax.open('GET', url, false);
+
+    ajax.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200){
+            let ajax = JSON.parse(this.responseText);
+            const avatar = ajax.avatar_url;
+            document.getElementById("github-avatar").src = avatar;
+        }
+    };
+
+    ajax.open('GET', url, true);
     ajax.send();
 
-    var data = ajax.responseText;
-
-    console.log(data.avatar_url);
 };
